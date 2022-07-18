ac6f037c836c875f54145d1dcac9f2a1a22b8b87
hata-tomoyuki <h.tomo0602@gmail.com>
2022-07-18 14:57:37 +0900

    setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '46f8869493953db6665932ef8acab580'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6f081796-da5b-447c-a130-44234ea38e87'
+};
